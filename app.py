7d0894f90436de1df00a0baf676638ca8b7cfb62
poojitha598 <poojithaahchary@gmail.com>
2026-05-23 15:20:25 +0530

    Fixed host binding for Render

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def index():
     return render_template('index.html', translated_text=translated_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
